eccfb1ffdd0f1909516f430ce135be84516ffdf6
Chintan Parikh <timtamboy63@gmail.com>
2012-03-30 18:02:40 +0800

    Updated and documented Config

--- engine/config.php
+++ engine/config.php
@@ -1,17 +1,46 @@
 <?php
-
+/**
+ * Config class - manages reading the configuration from the set config files (for both the global config file and components)
+ * The global config file must be an ini (unless the implementation of Config::get() is changed), and the component config files are all XML files
+ * 
+ * @version 1.0
+ * @author Chintan Parikh
+ *
+ * Changelist: 
+ */
 class Config
 {
+	/**
+	 * The location of the config directory
+	 */
+	const CONFIG_DIRECTORY = 'config/';
+
+	/**
+	 * The location of the component directory (must be under the config directory)
+	 */
+	const COMPONENT_DIRECTORY = 'components/'
+
+	/**
+	 * The filename of the global config file (located inside the config directry)
+	 */
+	const GLOBAL_CONFIG = 'config.ini';
+
+	/**
+	 * Gets a config item from the global config file and returns it
+	 */
 	public function get($item)
 	{
-		$config = parse_ini_file('config/config.ini');
+		$config = parse_ini_file(self::CONFIG_DIRECTORY . self::GLOBAL_CONFIG);
 		return $config[$item];
 	}
 
+	/**
+	 * Gets all config items from a component xml file
+	 */
 	public function getComponent($component)
 	{
 		$config = array();
-		$xml = simplexml_load_file('config/components/' . $component . '.config.shel');
+		$xml = simplexml_load_file((self::CONFIG_DIRECTORY . self::COMPONENT_DIRECTORY . $component . '.config.shel');
 
 		$vars = get_object_vars($xml);
 		foreach ($vars as $key=>$value)
